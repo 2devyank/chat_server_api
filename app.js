4d123b28aba9236f6c94f21d79911a1896a15a25
devyank <devyanknagpal2002@gmail.com>
2024-02-02 13:42:06 +0530

    cors origin changes

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cors(
         credentials:true, 
     }
 ))
-// app.use(cors())
+app.use(cors())
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
